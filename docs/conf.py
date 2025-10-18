a9a7d684949a6e85cace7b46ce3fe3dccb94cbbe
David <gtzdavid200@gmail.com>
2025-10-17 19:41:05 -0600

    Add initial Sphinx configuration for documentation

---
+++ docs/conf.py
@@ -0,0 +1,8 @@
+import os, sys
+sys.path.append(os.path.abspath('.'))
+project = 'Pronóstico de Cierre de Acción'
+author = 'Max Aguayo, Diego Canales, David Gutierrez'
+extensions = ['myst_parser']
+templates_path = ['_templates']
+exclude_patterns = []
+html_theme = 'sphinx_rtd_theme'
