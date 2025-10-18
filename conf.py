59e966dc1c816b3ed9cc31a589a05216604cd285
Diegocanales1 <canalesmdiegogmail.com>
2025-10-17 23:31:45 -0600

    update conf.py

--- conf.py
+++ conf.py
@@ -1,24 +1,63 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+from unittest.mock import Mock
+sys.path.insert(0, os.path.abspath('.'))
+
+MOCK_MODULES = [
+    'numpy', 'pandas', 'yfinance', 
+    'matplotlib', 'matplotlib.pyplot', 
+    'plotly', 'plotly.graph_objs', 'plotly.express', 
+    'statsmodels', 'statsmodels.tsa', 'statsmodels.tsa.statespace',
+    'statsmodels.tsa.statespace.sarimax', 
+    'sklearn', 'sklearn.preprocessing', 
+    'tensorflow', 'tensorflow.keras', 'tensorflow.keras.models', 
+    'tensorflow.keras.layers', 'tensorflow.keras.callbacks'
+]
+
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
 
 
 project = 'SARIMAX y FFNN para Predicción de Acciones'
 copyright = '2025, Max Aguayo, Diego Canales, David Gutierrez'
 author = 'Max Aguayo, Diego Canales, David Gutierrez'
 
-
+html_theme = 'sphinx_rtd_theme'
 extensions = [
     'sphinx.ext.autodoc', 
     'sphinx.ext.napoleon', 
-    'sphinx_rtd_theme',    
-    'myst_parser',       
+    'sphinx.ext.linkcode',    
+    'recommonmark',       
 ]
 
-source_suffix = ['.rst', '.md'] 
+def linkcode_resolve(domain, info):
+    if domain != 'py':
+        return None
+    if not info.get('module') or not info.get('fullname'):
+        return None
+    return None
 
-master_doc = 'index'
+html_static_path = ['_static']
 
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+autodoc_member_order = 'bysource'
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
 
-html_theme = 'sphinx_rtd_theme'
+html_show_sourcelink = False # Ocultar enlace a fuente
+html_show_sphinx = False # Ocultar footer de Sphinx
+
+html_context = {
+    "display_github": True,
+    "github_user": "DiegoCanales1", 
+    "github_repo": "SARIMAX-y-FFNN",
+    "github_version": "main", # o master
+    "conf_py_path": "/",
+}
+
+from recommonmark.parser import CommonMarkParser
+def setup(app):
+    app.add_source_suffix('.md', 'markdown')
+    app.add_config_value('recommonmark_config', {
+        'enable_eval_rst': True,
+    }, True)
+    app.add_source_parser(CommonMarkParser)
