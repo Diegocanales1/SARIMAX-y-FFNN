e176bdd4e9fe2061e10d11d4c7cdc146ec3e2588
Diegocanales1 <canalesmdiegogmail.com>
2025-10-17 23:08:03 -0600

    update config.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,36 +1,24 @@
-# -- Path setup --------------------------------------------------------------
-
 import os
 import sys
-# Asegúrate de que Sphinx pueda encontrar tu script de Python (prediccion_acciones.py)
 sys.path.insert(0, os.path.abspath('.'))
 
-# -- Project information -----------------------------------------------------
 
 project = 'SARIMAX y FFNN para Predicción de Acciones'
 copyright = '2025, Max Aguayo, Diego Canales, David Gutierrez'
 author = 'Max Aguayo, Diego Canales, David Gutierrez'
 
-# -- General configuration ---------------------------------------------------
 
 extensions = [
-    'sphinx.ext.autodoc',  # Para documentar código Python automáticamente
-    'sphinx.ext.napoleon', # Para soportar docstrings de NumPy/Google
-    'sphinx_rtd_theme',    # Tema Read the Docs
-    'myst_parser',         # Para que Sphinx pueda leer archivos Markdown (.md)
+    'sphinx.ext.autodoc', 
+    'sphinx.ext.napoleon', 
+    'sphinx_rtd_theme',    
+    'myst_parser',       
 ]
 
-# Fuentes de la documentación
-# Usamos solo '.md' y lo mapeamos a 'markdown'
 source_suffix = ['.rst', '.md'] 
 
-# La raíz de la documentación
-# Sphinx busca 'index' y encontrará index.md porque está en source_suffix
 master_doc = 'index'
 
-# Lista de patrones para ignorar
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-# -- Options for HTML output -------------------------------------------------
 
 html_theme = 'sphinx_rtd_theme'
